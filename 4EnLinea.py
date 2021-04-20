cbded482bed88d55f0a046075801c89442e924df
Guadalupe Biener <guadalupebiener@gmail.com>
2021-04-20 14:55:45 -0300

    Nuevas funciones para ver las filas/columnas

--- 4EnLinea.py
+++ 4EnLinea.py
@@ -48,4 +48,61 @@ def dibujarTablero(tablero):
 				print(' %s ' % celda, end=' ')
 		print(' ')
 
-dibujarTablero(completarTableroEnOrden(secuencia_1, tableroVacio()))
+tablero 1=dibujarTablero(completarTableroEnOrden(secuencia_1, tableroVacio()))
+
+def contenidoColumna(nro_columna,tablero)
+	columna=[]
+	for fila in tablero:
+		celda=fila[nro_columna - 1]
+		columna.append(celda)
+	return columna
+
+def llamadoColumna(nro,tablero)
+	if nro==0:
+		contenidoColumna(6,tablero)
+		contenidoColumna(5,tablero)
+		contenidoColumna(4,tablero)
+		contenidoColumna(3,tablero)
+		contenidoColumna(2,tablero)
+		contenidoColumna(1,tablero)
+	else: 
+		contenidoColumna(nro,tablero)
+
+def contenidoFila(nro_fila,tablero)
+	fila=[]
+	for columna in tablero:
+		celda=columna[nro_fila - 1]
+		fila.append(celda)
+	return fila
+
+def llamadoFila(nro,tablero)
+	if nro==0:
+		contenidoFila(6,tablero)
+		contenidoFila(5,tablero)
+		contenidoFila(4,tablero)
+		contenidoFila(3,tablero)
+		contenidoFila(2,tablero)
+		contenidoFila(1,tablero)
+	else: 
+		contenidoFila(nro,tablero)
+
+ver= int(input("Para ver las filas ingrese 1, para ver las columnas ingrese 2, para saltar esto precione otro numero"))
+
+if ver == 1:
+	num1=int(input("Ingrese 0 para ver todas las filas, o un numero del 1-6 para ver una en concreto"))
+	if num1 > 6:
+		print("Numero invalido")
+		return
+	else:
+		llamadoFila(num1,tablero1)
+else:
+	if ver == 2:
+		num2=int(input("Ingrese 0 para ver todas las columnas, o un numero del 1-6 para ver una en concreto"))
+		if num2 > 6:
+			print("Numero invalido")
+			return
+		else:
+			llamadoColumna(num2,tablero1)
+				
+	else:
+		print("saltado")
